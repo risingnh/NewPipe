204497612db3a8242d77e498b9eba756fbc96c96
risingnh <nafizhossain26@gmail.com>
2017-02-11 21:33:01 +0100

    add InfoItem

--- app/src/main/java/org/schabi/newpipe/extractor/stream_info/StreamInfoItem.java
+++ app/src/main/java/org/schabi/newpipe/extractor/stream_info/StreamInfoItem.java
@@ -1,13 +1,10 @@
 package org.schabi.newpipe.extractor.stream_info;
 
-import org.schabi.newpipe.extractor.AbstractStreamInfo;
-import org.schabi.newpipe.extractor.exceptions.ParsingException;
-
 /**
- * Created by Christian Schabesberger on 28.02.16.
+ * Created by Christian Schabesberger on 26.08.15.
  *
  * Copyright (C) Christian Schabesberger 2016 <chris.schabesberger@mailbox.org>
- * StreamPreviewInfoExtractor.java is part of NewPipe.
+ * StreamInfoItem.java is part of NewPipe.
  *
  * NewPipe is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,13 +20,9 @@ import org.schabi.newpipe.extractor.exceptions.ParsingException;
  * along with NewPipe.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-public interface StreamPreviewInfoExtractor {
-    AbstractStreamInfo.StreamType getStreamType() throws ParsingException;
-    String getWebPageUrl() throws ParsingException;
-    String getTitle() throws ParsingException;
-    int getDuration() throws ParsingException;
-    String getUploader() throws ParsingException;
-    String getUploadDate() throws ParsingException;
-    long getViewCount() throws  ParsingException;
-    String getThumbnailUrl() throws  ParsingException;
+import org.schabi.newpipe.extractor.AbstractStreamInfo;
+
+/**Info object for previews of unopened videos, eg search results, related videos*/
+public class StreamInfoItem extends AbstractStreamInfo {
+    public int duration;
 }
