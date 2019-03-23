c0937a26bc60af1b1d92e3ad6e5014465571ce27
risingnh <nafizhossain26@gmail.com>
2019-03-22 22:54:07 -0300

    New MP4 muxer + Queue changes + Storage fixes
    Main changes:
    * correctly check the available space (CircularFile.java)
    * misc cleanup (CircularFile.java)
    * use the "Error Reporter" for non-http errors
    * rewrite network state checking and add better support for API 21 (Lollipop) or higher
    * implement "metered networks"
    * add buttons in "Downloads" activity to start/pause all pending downloads, ignoring the queue flag or if the network is "metered"
    * add workaround for VPN connections and/or network switching. Example: switching WiFi to 3G
    * rewrite DataReader ¡Webm muxer is now 57% more faster!
    * rewrite CircularFile, use file buffers instead of memory buffers. Less troubles in low-end devices
    * fix missing offset for KaxCluster (WebMWriter.java), manifested as no thumbnails on file explorers
    
    Download queue:
    * remember queue status, unless the user pause the download (un-queue)
    * semi-automatic downloads, between networks. Effective if the user create a new download or the downloads activity is starts
    * allow enqueue failed downloads
    * new option, queue limit, enabled by default. Used to allow one or multiple downloads at same time
    
    Miscellaneous:
    * fix crash while selecting details/error menu (mistake on MissionFragment.java)
    * misc serialize changes (DownloadMission.java)
    * minor UI tweaks
    * allow overwrite paused downloads
    * fix wrong icons for grid/list button in downloads
    * add share option
    * implement #2006
    * correct misspelled word in strings.xml (es) (cmn)
    * fix MissionAdapter crash during device shutdown
    
    New Mp4Muxer + required changes:
    * new mp4 muxer (from dash only) with this, muxing on Android 7 is possible now!!!
    * re-work in SharpStream
    * drop mp4 dash muxer
    * misc changes: add warning in SecondaryStreamHelper.java,
    * strip m4a DASH files to normal m4a format (youtube only)
    
    Fix storage issues:
    * warn to the user if is choosing a "read only" download directory (for external SD Cards), useless is rooted :)
    * "write proof" allow post-processing resuming only if the device ran out of space
    * implement "insufficient storage" error for downloads

--- app/src/main/java/us/shandian/giga/postprocessing/Mp4FromDashMuxer.java
+++ app/src/main/java/us/shandian/giga/postprocessing/Mp4FromDashMuxer.java
@@ -1,6 +1,6 @@
 package us.shandian.giga.postprocessing;
 
-import org.schabi.newpipe.streams.Mp4DashWriter;
+import org.schabi.newpipe.streams.Mp4FromDashWriter;
 import org.schabi.newpipe.streams.io.SharpStream;
 
 import java.io.IOException;
@@ -10,15 +10,15 @@ import us.shandian.giga.get.DownloadMission;
 /**
  * @author kapodamy
  */
-class Mp4DashMuxer extends Postprocessing {
+class Mp4FromDashMuxer extends Postprocessing {
 
-    Mp4DashMuxer(DownloadMission mission) {
-        super(mission, 15360 * 1024/* 15 MiB */, true);
+    Mp4FromDashMuxer(DownloadMission mission) {
+        super(mission, 2 * 1024 * 1024/* 2 MiB */, true);
     }
 
     @Override
     int process(SharpStream out, SharpStream... sources) throws IOException {
-        Mp4DashWriter muxer = new Mp4DashWriter(sources);
+        Mp4FromDashWriter muxer = new Mp4FromDashWriter(sources);
         muxer.parseSources();
         muxer.selectTracks(0, 0);
         muxer.build(out);
