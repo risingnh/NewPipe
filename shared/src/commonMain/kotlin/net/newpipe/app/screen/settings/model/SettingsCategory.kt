e8fdb0ca2badb0aa073851d92b6713b24fa9093f
risingnh <nafizhossain26@gmail.com>
2026-07-02 10:49:25 +0100

    add settings homescreen tests

--- shared/src/commonMain/kotlin/net/newpipe/app/screen/settings/model/SettingsCategory.kt
+++ shared/src/commonMain/kotlin/net/newpipe/app/screen/settings/model/SettingsCategory.kt
@@ -1,3 +1,8 @@
+/*
+* SPDX-FileCopyrightText: 2026 NewPipe e.V. <https://newpipe-ev.de>
+* SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 package net.newpipe.app.screen.settings.model
 
 import org.jetbrains.compose.resources.DrawableResource
